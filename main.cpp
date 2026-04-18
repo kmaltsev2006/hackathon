ba6cbfce82c016c11496a53e6b3f5d32aaf01fb5
Spookich <yagudin-05@mail.ru>
2026-04-18 13:47:08 +0300

    обработка входных аргументов

--- main.cpp
+++ main.cpp
@@ -2,7 +2,40 @@
  * @license
  * (C) PROTEI protei.ru
  */
+#include <iostream>
+#include <unistd.h>
+#include <getopt.h>
+
+const struct option long_options[] = {
+    {"input", required_argument, nullptr, 'i'},
+    {"output", required_argument, nullptr, 'o'},
+    {"help", no_argument, nullptr, 'h'},
+    {nullptr, 0, nullptr, 0}
+};
 
 int main(int argc, char* argv[]) {
-  return 0;
+  std::string input_filename, output_filename = "a.csv";
+
+  int opt, opt_index = 0;
+
+  if (argc == 1) {
+    std::cout << "-i --input:   имя входного файла\n-o --output:   имя выходного файла\n";
+  }else{
+    while((opt = getopt_long(argc, argv, "i:o:h", long_options, &opt_index)) != -1){
+      switch(opt){
+        case 'i':
+          input_filename = optarg;
+          break;
+        case 'o':
+          output_filename = optarg;
+          break;
+        case 'h':
+          std::cout << "-i --input:   имя входного файла\n-o --output:   имя выходного файла\n";
+          break;
+      }
+    }
+  }
+
+  std::cout << "input name: " << input_filename << std::endl << "output name: " << output_filename << std::endl;
+
 }
